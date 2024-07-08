fa4c5766edc8a0b3b9a7ce96148c8351bf1e04a7
sheriff <chege2452@gmail.com>
2024-07-08 14:33:42 +0300

    i added some function in index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 2;  
+let num2 = 31; 
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 100) + 1; 
+
+
+let num3 = 14; 
+let num4 = 5;  
+let mod = num3 % num4;
+
+
+let max = Math.max(1, 5, 10, 20); 
+
+
+console.log("Multiply:", multiply); 
+console.log("Random:", random);     
+console.log("Mod:", mod);          
+console.log("Max:", max);           
